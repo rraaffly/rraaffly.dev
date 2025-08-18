ae19e71e366415f82c63d541e020a2842420d4c7
rraaffly <muh.raflyrafsanjani@gmail.com>
2025-08-19 00:48:09 +0700

    chore: component container

---
+++ src/lib/components/ui/Container.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { cn } from '$lib/utils/helper';
+
+	let { children, class: className = '', ...rest } = $props();
+</script>
+
+<div class={cn('mx-auto w-full px-6 lg:w-9/12', className)} {...rest}>{@render children?.()}</div>
