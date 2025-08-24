9750a79ee54ab8de98ee3d668db4f1b49ad6fb15
rraaffly <muh.raflyrafsanjani@gmail.com>
2025-08-24 11:09:02 +0700

    chore: add new skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -4,19 +4,51 @@ export const skills = [
 		icon: 'logos:python'
 	},
 	{
-		title: 'TypeScript',
-		icon: 'logos:typescript-icon'
+		title: 'Django',
+		icon: 'logos:django-icon'
 	},
 	{
-		title: 'React',
-		icon: 'logos:react'
+		title: 'FastAPI',
+		icon: 'logos:fastapi-icon'
+	},
+	{
+		title: 'TypeScript',
+		icon: 'logos:typescript-icon'
 	},
 	{
 		title: 'Bun',
 		icon: 'logos:bun'
 	},
 	{
-		title: 'Postgresql',
+		title: 'Next.js',
+		icon: 'logos:nextjs-icon'
+	},
+	{
+		title: 'Svelte',
+		icon: 'logos:svelte-icon'
+	},
+	{
+		title: 'MySQL',
+		icon: 'logos:mysql'
+	},
+	{
+		title: 'PostgreSQL',
 		icon: 'logos:postgresql'
+	},
+	{
+		title: 'MongoDB',
+		icon: 'logos:mongodb-icon'
+	},
+	{
+		title: 'Redis',
+		icon: 'logos:redis'
+	},
+	{
+		title: 'Docker',
+		icon: 'logos:docker-icon'
+	},
+	{
+		title: 'Git',
+		icon: 'logos:git-icon'
 	}
 ];
