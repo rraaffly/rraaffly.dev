1a05a490fbaec2f90b773a8a9c07e315d5180c37
rraaffly <muh.raflyrafsanjani@gmail.com>
2025-08-25 20:41:12 +0700

    chore: update data projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,30 +1,62 @@
-import type { Device } from '$lib/utils/types';
+import type { Platform } from '$lib/utils/types';
 
-export const projects: Array<{
+export type Tool =
+	| 'svelte'
+	| 'react'
+	| 'django'
+	| 'fastapi'
+	| 'python'
+	| 'typescript'
+	| 'postgresql'
+	| 'firebase'
+	| 'xray'
+	| 'katalon'
+	| 'mysql'
+	| 'redhat'
+	| 'sqlite';
+
+export type ProjectType =
+	| 'Web App Development'
+	| 'Mobile App Development'
+	| 'Desktop App Development'
+	| 'API'
+	| 'Automation Testing';
+
+export type ProjectData = {
 	img: string;
 	title: string;
+	type: ProjectType;
 	description: string;
-	device: Device[];
-}> = [
+	platform: Platform[];
+	tool: Tool[];
+};
+
+export const projects: ProjectData[] = [
 	{
 		img: 'https://dummyimage.com/800x600/000/fff',
-		title: 'ShutterLab',
+		title: 'HCMS 4.0',
 		description:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea blanditiis dolor nostrum, quos beatae magni iusto libero dolorem architecto quidem!',
-		device: ['web']
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea blanditiis dolor nostrum, quos beatae magni iusto!',
+		platform: ['web'],
+		tool: ['katalon', 'mysql', 'redhat', 'xray'],
+		type: 'Automation Testing'
 	},
 	{
 		img: 'https://dummyimage.com/800x600/000/fff',
-		title: 'ShutterLab Node Agent',
+		title: 'Genetic Algorithm',
 		description:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea blanditiis dolor nostrum, quos beatae magni iusto libero dolorem architecto quidem!',
-		device: ['shell', 'web']
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea blanditiis dolor nostrum, quos beatae magni iusto!',
+		platform: ['api'],
+		tool: ['python', 'fastapi', 'sqlite'],
+		type: 'API'
 	},
 	{
 		img: 'https://dummyimage.com/800x600/000/fff',
 		title: 'NutriMom',
 		description:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea blanditiis dolor nostrum, quos beatae magni iusto libero dolorem architecto quidem!',
-		device: ['mobile']
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea blanditiis dolor nostrum, quos beatae magni iusto!',
+		platform: ['mobile'],
+		tool: ['typescript', 'react', 'firebase'],
+		type: 'Mobile App Development'
 	}
 ];
